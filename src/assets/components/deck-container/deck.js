f0be852950a5b88170d7b76a190c82ebed47c038
Buffik <Som16@mail.ru>
2022-08-29 02:26:13 +0300

    deploy

--- src/assets/components/deck-container/deck.js
+++
@@ -1,6 +0,0 @@
-let renderDeck = (elem, parent) => {
-  elem.classList.add('deck')
-  parent.appendChild(elem)
-}
-
-export default renderDeck
--- src/assets/components/deck-container/last-card.js
+++
@@ -1,8 +0,0 @@
-import { brownCards, blueCards, greenCards } from '../../../data/mythicCards/index'
-
-let renderLastCard = (elem, parent) => {
-  elem.classList.add('last-card')
-  parent.appendChild(elem)
-}
-
-export default renderLastCard
